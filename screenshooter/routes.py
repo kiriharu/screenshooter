1959edb8949bd3d3d22ca1b5e4352ebc0d745dbf
kiriharu <kiriharu@yandex.ru>
2021-09-28 19:12:34 +0300

    pic_type choosing (both jpeg and png)

--- screenshooter/routes.py
+++ screenshooter/routes.py
@@ -6,7 +6,7 @@ from pydantic import HttpUrl
 from starlette.responses import StreamingResponse
 
 from screenshooter.schemas import BrowserSettings
-from screenshooter.screenshot import Screenshot
+from screenshooter.screenshot import Screenshot, PicType
 
 main_router = APIRouter()
 
@@ -14,6 +14,7 @@ main_router = APIRouter()
 @main_router.get("/screenshot")
 async def screenshoot(
     url: HttpUrl,
+    pic_type: PicType,
     width: Optional[int] = Query(800, lt=2000),
     height: Optional[int] = Query(600, lt=2000),
     isMobile: Optional[bool] = False,
@@ -23,7 +24,7 @@ async def screenshoot(
     screenshot_obj = Screenshot(str(url), BrowserSettings(
         width=width, height=height, isMobile=isMobile,
         deviceScaleFactor=deviceScaleFactor, isLandscape=isLandscape
-    ))
+    ), pic_type)
     async with screenshot_obj as s:
         binary = await s.get_binary_screenshot()
-        return StreamingResponse(content=BytesIO(binary), media_type="image/png")
+        return StreamingResponse(content=BytesIO(binary), media_type=f"image/{pic_type.value}")
